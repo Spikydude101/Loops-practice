603af0326c59dba1790e570a19fa911c90857e21
Spikydude101 <55212158+Spikydude101@users.noreply.github.com>
2019-10-08 17:32:15 -0600

    Add files via upload

---
+++ looping-practice/main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//looping
+
+
+for(let message = 0; message < 500; message+=1  ) {
+    console.log("I'm so happy!")
+}
+
+for( let multi = 4;  multi < 800; multi*4) {
+    console.log('multiples!')
+}
+
+
+for(let odd = 55; odd>11; odd-2) {
+    console.log('odd numbers!')
+}
+let num1 = 0;
+for (let adding1 = 5; adding1 < 50; adding1 +=1 ) {
+    num += adding1;
+    console.log('sum of all addition is:' + num);
+}
+let num2 = 0;
+for(let adding2 = 10; adding2 <=100; adding2 += 10) {
+    num2 += adding2;
+}
+conhsole.log('sum of the whole thing is:' + num2);
